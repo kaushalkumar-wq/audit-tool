818cf125ce756f7142c107234e740f614dca46a2
kaushalkumar-wq <kaushal.kumar@vetic.in>
2026-04-17 11:59:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+from flask import Flask, render_template, request, redirect, session
 
+app = Flask(__name__)
+app.secret_key = "vetic-secret-key"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "kaushal" and password == "12345":
+            session["user"] = username
+            return redirect("/dashboard")
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect("/")
+    return render_template("dashboard.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run()
